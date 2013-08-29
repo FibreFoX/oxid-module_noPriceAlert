1a865186e86738151588a182dbc989da1a1f95ae
Danny Althoff (aka FibreFoX) <fibrefox@dynamicfiles.de>
2013-08-29 13:11:19 +0200

    initial import, every module will be in its own git-repo

---
+++ 4.6.x/dynamicfiles/noPriceAlert/metadata.php
@@ -0,0 +1,29 @@
+<?php
+/**
+Copyright (c) 2013 Danny Althoff
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+    Author: Danny Althoff
+	Website: http://www.dynamicfiles.de
+	Contact: info@dynamicfiles.de
+*/
+$sMetadataVersion = '1.0';
+
+$aModule = array(
+    'id'           => 'noPriceAlert',
+    'title'        => 'Anti-Preisalarm',
+    'description'  => 'Deaktiviert das Preisalarm-Feature im Frontend (OXID 4.6.x).',
+    'version'      => '1.0',
+    'author'       => 'Danny Althoff',
+    'url'          => 'http://www.dynamicfiles.de.de',
+    'email'        => 'info@dynamicfiles.de',
+    'extend'       => array(
+        'details'					=> 'dynamicfiles/noPriceAlert/noPriceAlertDetails',
+    ),
+);
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-oxid-module_noPriceAlert
-========================
+Module für OXID eShop-Systeme: Preisalarm deaktivieren
+============
+
+Hauptsächlich für die CE-Versionen unterteilt nach OXID-Versionen, es sollte aber keine Probleme in PE/EE-Versionen des Shops bereiten.
+
+Alle Module ohne jegliche Gewähr. Alle über Github abrufbaren Module sind in der jeweiligen Fassung mit der MIT-Lizenz freigegeben. Eine separate und/oder parallele Lizensierung wird vorbehalten.
+
+Wenn ihr meine Module einsetzt, wäre ich euch sehr sehr sehr dankbar, ich würde extra dafür eine Liste führen. Sollte jemand mir Feedback geben wollen, so könnt ihr einfach an meine Mailadresse (siehe Github-Profil) eine Nachricht (am Besten mit einem aussagekräftigen Titel bezüglich OXID) schicken.
